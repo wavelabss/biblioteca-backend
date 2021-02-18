ca65f7a32b0525644b2a9e0a018a94d6c3a9e823
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-02-18 17:30:34 -0300

    feat: add graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,15 @@ try {
 
   server.init()
   server.start()
+
+  const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT']
+
+  exitSignals.forEach(signal => {
+    process.on(signal, () => {
+      console.log('App exited with success')
+      process.exit(ExitStatus.Success)
+    })
+  })
 } catch (error) {
   console.error(`App exited with error: ${error}`)
   process.exit(ExitStatus.Failure)

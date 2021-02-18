a75c9741d3704001d29ca649c92ca1bcec82ee2d
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-02-18 17:22:22 -0300

    feat: add setup server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-import './module-alias'
-console.log('running...')
+import { SetupServer } from './server'
+
+enum ExitStatus {
+  Failure = 1,
+  Success = 0
+}
+
+try {
+  const server = new SetupServer(3333)
+
+  server.init()
+  server.start()
+} catch (error) {
+  console.error(`App exited with error: ${error}`)
+  process.exit(ExitStatus.Failure)
+}

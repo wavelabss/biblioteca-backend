86a68914476776c857b4f012523ce25dc5b1a8f5
Bruno Laviso <bruno.laviso@hotmail.com>
2021-02-21 16:16:39 -0300

    feat: add PUT books route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,6 +8,7 @@ const booksController = new BooksController()
 
 router.get('/books', booksController.index)
 router.post('/books', booksController.store)
+router.put('/books/:id', booksController.update)
 router.delete('/books/:id', booksController.destroy)
 
 export default router

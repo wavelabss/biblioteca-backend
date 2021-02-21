69ee9fdceaa56fce0d33af0459a5c75bc08960c9
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-02-21 15:47:30 -0300

    feat: return registered books

--- src/controllers/books.ts
+++ src/controllers/books.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express'
+import { Book } from '@src/models/book'
 
 export class BooksController {
-  public async index(request: Request, response: Response): Promise<Response> {
-    return response.json({ hello: 'Hello' })
+  public async index(_: Request, response: Response): Promise<Response> {
+    try {
+      const books = await Book.find()
+      return response.status(200).json(books)
+    } catch (error) {
+      return response.status(500).json({
+        code: 500,
+        message: 'Something went wrong'
+      })
+    }
   }
 }
